6c2f82674d4b7091fa725c42d521fd8a287df70b
Russell Jones <jonesrussell42@gmail.com>
2018-04-17 07:36:42 -0400

    Renamed clicked to flag-operation

--- js/views_process_flag_page.js
+++ js/views_process_flag_page.js
@@ -2,19 +2,45 @@
 
     'use strict';
 
+    /**
+     * Add Process Flag functionality to views table rows
+     */
     Drupal.behaviors.viewsRowColor = {
-        attach: function (context) {
-
-            var link = $(context).find('.clicked');
-             link.each(function (e) {
-               if ($(this).hasClass('green')) {
-                    $(this).parents("tr").toggleClass("highlight-green");
+        attach: function (context, settings) {
+            // Iterate over all Process Flag operations links
+            $('.flag-operation', context).once('viewsRowColor').each(function () {
+                // Update parent row colors
+                if ($(this).hasClass('green')) {
+                    $(this).parents("tr").addClass("highlight-green");
                 }
-               else if ($(this).hasClass('red')) {
-                    $(this).parents("tr").toggleClass("highlight-red");
+                else if ($(this).hasClass('red')) {
+                    $(this).parents("tr").addClass("highlight-red");
                 }
-              });
 
+                // Create an AJAX link to change Process Flag value
+                $(this).click(function(){ toggleRow(this); });
+            });
         }
     }
+
+    /**
+     * Update table rows / process flag operations links
+     * 
+     * @param {object} row 
+     */
+    function toggleRow(row) {
+        $(row).toggleClass('green red');
+
+        if ($(row).hasClass('green')) {
+            $(row).parents("tr").addClass("highlight-green");
+            $(row).parents("tr").removeClass("highlight-red");
+            $(row).text('Yes').fadeIn();
+        }
+        else {
+            $(row).parents("tr").addClass("highlight-red");
+            $(row).parents("tr").removeClass("highlight-green");
+            $(row).text('No').fadeIn();
+        }
+    }
+
 })(jQuery, Drupal);
